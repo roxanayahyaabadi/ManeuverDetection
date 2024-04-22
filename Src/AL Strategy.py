1f3dcf866f1caebcbefdc0087c4505bb0062e712
roxanayahyaabadi <104534637+roxanayahyaabadi@users.noreply.github.com>
2024-04-22 13:37:06 -0400

    Update AL Strategy.py

--- Src/AL Strategy.py
+++ Src/AL Strategy.py
@@ -1,11 +1,11 @@
 import pandas as pd
 
 # Load the combined CSV file with all samples, including final maneuver types and confidence scores
-csv_path = 'C:/Users/roxan/OneDrive - The University of Western Ontario/ITSC/driver_19/Weighted/Third/all_samples_with_final_labels.csv'
+csv_path = '.../all_samples_with_final_labels.csv'
 data = pd.read_csv(csv_path)
 
 # Specify the number of frames with the lowest confidence scores you want to list
-n = 1000  # You can change this number
+n = 3000  # You can change this number
 
 # Filter to include only test set data
 test_data = data[data['Data_Type'] == 'Test']
@@ -20,7 +20,7 @@ lowest_confidence_frames = sorted_test_data_by_confidence.head(n)
 lowest_confidence_frames_sorted = lowest_confidence_frames.sort_values(by='frame_id', ascending=True)[['frame_id', 'max_proba']]
 
 # Save these frames to a CSV file
-output_csv_path = 'C:/Users/roxan/OneDrive - The University of Western Ontario/ITSC/driver_19/Weighted/Third/lowest_confidence_iter_1.csv'
+output_csv_path = '.../lowest_confidence_iter_1.csv'
 lowest_confidence_frames_sorted.to_csv(output_csv_path, index=False)
 
 print(f"The {n} frames with the lowest confidence scores, sorted by frame_id, have been saved to {output_csv_path}.")
